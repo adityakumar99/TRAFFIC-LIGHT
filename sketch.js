cb79eb47eab7f9ad9b6231853c723a8cda5770aa
Aditya Kumar <90300375+adityakumar99@users.noreply.github.com>
2021-11-25 17:39:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+   //made by aditya 
+          //hope you enjoy :D
+
+
+
+let redLight = 'red'
+let yellowLight = 'yellow'
+let greenLight = 'green'
+
+function setup() {
+  createCanvas(200,200)
+}
+                
+function draw() {
+  background(220);
+  rectMode(CENTER)
+  fill(99,99,999);
+  rect(width / 2, height / 2, 80, 160);
+
+  fill(redLight)
+  ellipse(width / 2, height / 2 - 50, 40, 40)
+
+  fill(yellowLight)
+  ellipse(width / 2, height / 2, 40)
+
+  fill(greenLight)
+  ellipse(width / 2, height / 2 + 50, 40, 40)
+
+                   
+
+
+  if (mouseY < height/3) {
+  redLight ='red'
+  yellowLight = 'white'
+  greenLight = 'white'
+  
+  }
+  else if (mouseY < height * 2/3) {
+  redLight ='white'
+  yellowLight = 'yellow'
+  greenLight = 'white'
+  }
+    else if (mouseY < height) {
+  redLight ='white'
+  yellowLight = 'white'
+  greenLight = 'green'
+  }
+}
